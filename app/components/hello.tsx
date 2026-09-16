527015724173dadf7e2320246606aae77d8f648d
Flavio Roman Goncalez <romanflavio3@hotmail.com>
2026-09-15 23:02:40 -0300

    Feat: distinguir SSR component y CSR component

---
+++ app/components/hello.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+const Hello = () => {
+    console.log("Soy un componente del cliente (navegador)")
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Hello
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Hello from "./components/hello"
+
 const Home = () => {
+  console.log("Que tipo de componente soy?")
   return (
-    <div className="text-5xl">Welcome to Next.js!</div>
+    <main>
+      <div className="text-5xl">Welcome to Next.js!</div>
+      <Hello/>
+    </main>
   )
 }
 
